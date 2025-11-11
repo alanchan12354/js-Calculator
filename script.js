4bc01109da3e141d85532a5f8075e3c54a8ddc73
CheungHong CHAN <67678585+alanchan12354@users.noreply.github.com>
2025-11-11 16:49:13 +0800

    Add calculator buttons and implement display functions

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ let a;
 let b;
 let operator;
 
+const buttons = document.querySelector(".buttons");
+const display = document.querySelector(".display");
+
+// returns (number)
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
+// returns (number)
 const operate = (a, b, operator) => {
     switch (operator) {
         case "+":
@@ -21,3 +26,29 @@ const operate = (a, b, operator) => {
             alert("ERROR");
     }
 }
+
+// add text to display
+const displayAdd = (str) => {
+    // Prevent multiple decimal points
+    if (display.textContent.includes(".") && str === ".") {
+        return;
+    }
+    display.textContent += str;
+}
+
+// clear display
+const clearDisplay = () => {
+    display.textContent = "";
+}
+
+buttons.addEventListener("click", e => {
+    let btnType = e.target.classList;
+    switch (true) {
+        case (btnType.contains("digit")):
+            displayAdd(e.target.dataset.value);
+            break;
+        case (btnType.contains("operator")):
+            
+            
+    }
+})
